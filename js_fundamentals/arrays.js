fa08fcc0f457c6fedc3088d62c808ee66a388cdf
Graham Lavender <glavender@gmail.com>
2013-11-21 12:41:46 -0500

    Completed arrays exercise

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -6,5 +6,23 @@
 // For each choice, log to the screen a string like: "My #1 choice is blue."
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
 
+var topCities = ["Toronto", "Montreal", "Vancouver", "Ottawa", "Halifax"];
+for (var i=0; i < topCities.length; i++) {
+  switch(i + 1)
+  {
+  case 1:
+  choice = "1st";
+  break;
+  case 2:
+  choice = "2nd";
+  break;
+  case 3:
+  choice = "3rd"
+  break;  
+  default:
+  choice = (i + 1) + "th";
+  }
 
+  console.log("My " + choice + " choice is " + topCities[i])
+}
 
